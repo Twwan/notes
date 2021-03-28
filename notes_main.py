67000305db80ca8f9ea311cfd2c850e963e983af
Twwan <65574497+Twwan@users.noreply.github.com>
2021-03-28 18:12:50 +0500

    Add files via upload

--- notes_main.py
+++ notes_main.py
@@ -1,63 +1,15 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QMessageBox, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
-import json
-def show_note():
-    key = list_notes.selectedItems()[0].text()
-    print(key)
-    field_text.setText(notes[key]['текст'])
-    list_tags.clear()
-    list_tags.addItems(notes[key]['теги'])
-def create_note():
-    note_name, result = QInputDialog.getText(main_win, "Добавить заметку", "Название заметки:")
-    if result == True:
-        list_notes.addItem(note_name)
-        notes[note_name] = {'текст': '', 'теги': []}
-def save_notes():
-    name = list_notes.selectedItems()[0].text()
-    notes[name]['текст'] = field_text.toPlainText()
-    with open('notes.json', 'w', encoding='utf-8') as file:
-        json.dump(notes, file, sort_keys=True, ensure_ascii=False)
-def del_notes():
-    name = list_notes.selectedItems()[0].text()
-    del notes[name]
-    list_notes.clear()
-    list_notes.addItems(notes)
-    with open('notes.json', 'w', encoding='utf-8') as file:
-        json.dump(notes, file, sort_keys=True, ensure_ascii=False)
-def add_tag():
-    name = list_notes.selectedItems()[0].text()
-    tag = field_tag.text()
-    if tag in notes[name]['теги']:
-        nope = QMessageBox()
-        nope.setText('Данный тег в выбранной заметке существует.')
-        nope.exec_()
-    else:
-        notes[name]['теги'].append(tag)
-        with open('notes.json', 'w', encoding='utf-8') as file:
-            json.dump(notes, file, sort_keys=True, ensure_ascii=False)
-        show_note()
-def del_tag():
-    name = list_notes.selectedItems()[0].text()
-    tag = list_tags.selectedItems()[0].text()
-    notes[name]['теги'].remove(tag)
-    with open('notes.json', 'w', encoding='utf-8') as file:
-        json.dump(notes, file, sort_keys=True, ensure_ascii=False)
-    show_note()
-def search_tag():
-    search_notes.clear()
-    tag = field_tag.text()
-    for note in notes:
-        if tag in notes[note]['теги']:
-            search_notes.append(note)
-    list_notes.clear()
-    list_notes.addItems(search_notes)
-
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
+ 
 app = QApplication([])
-app.setStyle('Fusion')
-main_win = QWidget()
-main_win.resize(900, 600)
-main_win.setWindowTitle('Заметки')
+notes = []
+ 
+'''Интерфейс приложения'''
+#параметры окна приложения
+notes_win = QWidget()
+notes_win.setWindowTitle('Умные заметки')
+notes_win.resize(900, 600)
+ 
 #виджеты окна приложения
 list_notes = QListWidget()
 list_notes_label = QLabel('Список заметок')
@@ -69,14 +21,12 @@ button_note_save = QPushButton('Сохранить заметку')
 field_tag = QLineEdit('')
 field_tag.setPlaceholderText('Введите тег...')
 field_text = QTextEdit()
-button_add = QPushButton('Добавить к заметке')
-button_del = QPushButton('Открепить от заметки')
-button_search = QPushButton('Искать заметки по тегу')
+button_tag_add = QPushButton('Добавить к заметке')
+button_tag_del = QPushButton('Открепить от заметки')
+button_tag_search = QPushButton('Искать заметки по тегу')
 list_tags = QListWidget()
 list_tags_label = QLabel('Список тегов')
  
-
-
 #расположение виджетов по лэйаутам
 layout_notes = QHBoxLayout()
 col_1 = QVBoxLayout()
@@ -97,48 +47,87 @@ col_2.addWidget(list_tags_label)
 col_2.addWidget(list_tags)
 col_2.addWidget(field_tag)
 row_3 = QHBoxLayout()
-row_3.addWidget(button_add)
-row_3.addWidget(button_del)
+row_3.addWidget(button_tag_add)
+row_3.addWidget(button_tag_del)
 row_4 = QHBoxLayout()
-row_4.addWidget(button_search)
+row_4.addWidget(button_tag_search)
  
 col_2.addLayout(row_3)
 col_2.addLayout(row_4)
  
 layout_notes.addLayout(col_1, stretch = 2)
 layout_notes.addLayout(col_2, stretch = 1)
-main_win.setLayout(layout_notes)
-#
-btn_del = QPushButton('Удалить')
-
-search_notes = []
-
-
-notes = {
-            'Заметка':
-                {
-                    'текст': 'Очень важная заметка',
-                    'теги': ['тег 1', 'тег 2']
-                }
-        }
-
-#with open('notes.json', 'w', encoding='utf-8') as file:
-#    json.dump(notes, file, sort_keys=True, ensure_ascii=False)
-
-notes = []
-    
-with open('notes.json', 'r', encoding='utf-8') as file:
-    notes = json.load(file)
-
-list_notes.addItems(notes)
-
-
+notes_win.setLayout(layout_notes)
+ 
+'''Функционал приложения'''
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    print(key)
+    for note in notes:
+        if note[0] == key:
+            field_text.setText(note[1])
+            list_tags.clear()
+            list_tags.addItems(note[2])
+ 
+def add_note():
+    note_name, ok = QInputDialog.getText(notes_win, "Добавить заметку", "Название заметки: ")
+    if ok and note_name != "":
+        note = list()
+        note = [note_name, '', []]
+        notes.append(note)
+        list_notes.addItem(note[0])
+        list_tags.addItems(note[2])
+        print(notes)
+        with open(str(len(notes)-1)+".txt", "w") as file:
+            file.write(note[0]+'\n')
+ 
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        index = 0
+        for note in notes:
+            if note[0] == key:
+                note[1] = field_text.toPlainText()
+                with open(str(index)+".txt", "w") as file:
+                    file.write(note[0]+'\n')
+                    file.write(note[1]+'\n')
+                    for tag in note[2]:
+                        file.write(tag+' ')
+                    file.write('\n')
+            index += 1
+        print(notes)
+    else:
+        print("Заметка для сохранения не выбрана!")
+ 
+#обработка событий
 list_notes.itemClicked.connect(show_note)
-button_note_create.clicked.connect(create_note)
-button_note_save.clicked.connect(save_notes)
-button_note_del.clicked.connect(del_notes)
-button_add.clicked.connect(add_tag)
-button_del.clicked.connect(del_tag)
-button_search.clicked.connect(search_tag)
-main_win.show()
+button_note_create.clicked.connect(add_note)
+button_note_save.clicked.connect(save_note)
+ 
+#запуск приложения 
+notes_win.show()
+ 
+name = 0
+note = []
+while True:
+    filename = str(name)+".txt"
+    try:
+        with open(filename, "r") as file:
+            for line in file:
+                line = line.replace('\n', '')
+                note.append(line)
+        tags = note[2].split(' ')
+        note[2] = tags
+        
+        notes.append(note)
+        note = []
+        name += 1
+ 
+    except IOError:
+        break
+ 
+print(notes)
+for note in notes:
+    list_notes.addItem(note[0])
+ 
 app.exec_()
